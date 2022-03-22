2f1e862b9c315e54ae9ae723e58a23a7ec370810
6jx <70932827+6jx@users.noreply.github.com>
2022-03-22 16:06:13 -0400

    begins

---
+++ init.js
@@ -0,0 +1,17 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS,  Intents.FLAGS.GUILD_MESSAGES] });
+const { autopurgechannel } = require('./config.json');
+const { messagemaximum } = require('./config.json');
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+var msgcounter = 0,
+client.on('message', message => {
+	if(message.channel.id == autopurgechannel){nsfwchat++};
+	if(msgcounter >= messagemaximum){
+	message.channel.bulkDelete(messagemaximum);
+	nsfwchat = 0;
+}});
+client.login(token);
+
